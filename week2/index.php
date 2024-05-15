7f825a9a0157e8698e0516de0645e9e759484794
Jinil Parekh <59524410+jinil2401@users.noreply.github.com>
2024-05-15 17:39:48 -0400

    lorem ipsum

---
+++ week2/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <?php echo 'hello'; ?>
+    <?php echo '<h1> Welcome to HTTP 5225</h1>' ?>
+    <?php echo '<p> We will learn PHP In this class. </p>'?>
+    <?php 
+    echo '<ul>
+     <li>PHP</li>
+     <li>MYSQL</li>
+     <li>LARAVEL</li>
+    </ul>' ; 
+    ?>
+    <?php
+        echo'<img src = "https://placehold.co/600x400" alt="placeholder image">';
+    ?>
+
+<?php
+$fname = 'Jinil';
+$lname = 'Parekh';
+?>
+
+<?php
+    echo '<br>';
+    echo 'Hello'.' '.$fname .' '.$lname;
+?>
+    <h1>Welcome to HTTP 5225 </h1>
+    <p>We will learn  PHP in this class</p>
+<ul>
+    <li>PHP</li>
+    <li>MYSQL</li>
+    <li>LARAVEL</li>
+</ul>
+
+<?php
+echo'<img src = "https://placehold.co/600x400" alt="placeholder image">';
+?>
+</body>
+</html>
